4395e3c552471fa1ef2661461b9e4716b8e6c370
Dzanan Aliman <dzananaliman13@gmail.com>
2025-09-18 11:25:39 +0300

    Add home controller

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Webside with JS real project for client
 3. Setup handlebars
 4. Add resources
 5. Setup static route
+6. Render home page
+7. Add routes and controllers
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 
+import homeController from './controllers/homeController.js';
+
 const app = express();
 
 app.engine('hbs', handlebars.engine({
@@ -9,10 +11,10 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
-app.use(express.static('src/public'));
+app.use(express.static('public'));
+
+app.use(homeController);
 
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
